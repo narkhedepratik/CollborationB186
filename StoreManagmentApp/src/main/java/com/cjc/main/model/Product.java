cd198d2c1b97df044cc0ccb6cd6eddcba383a662
prati <prati@Pratik>
2025-03-03 10:26:01 +0530

    StoreManagmentApp Structure

---
+++ StoreManagmentApp/src/main/java/com/cjc/main/model/Product.java
@@ -0,0 +1,35 @@
+package com.cjc.main.model;
+
+public class Product {
+	
+	private int productId;
+	private String productName;
+	private float price;
+	private String categoryl;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getCategoryl() {
+		return categoryl;
+	}
+	public void setCategoryl(String categoryl) {
+		this.categoryl = categoryl;
+	}
+	
+
+}
