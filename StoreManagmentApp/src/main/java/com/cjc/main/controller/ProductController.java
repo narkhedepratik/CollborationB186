324e2b7ef3aa82f321b3d9026bf53f089915e7e9
prati <prati@Pratik>
2025-03-03 11:01:56 +0530

    Added get all api

--- StoreManagmentApp/src/main/java/com/cjc/main/controller/ProductController.java
+++ StoreManagmentApp/src/main/java/com/cjc/main/controller/ProductController.java
@@ -1,8 +1,17 @@
 package com.cjc.main.controller;
-
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.cjc.main.model.Product;
 
 @RestController
 public class ProductController {
-
+	
+	
+@GetMapping("get_products")
+public List<Product> getAllProducts()
+{
+	return new ArrayList<>();
+}
 }
