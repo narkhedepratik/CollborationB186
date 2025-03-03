2681eac0ad5b4d52c651b2f7c6adb742ff3a4685
ProgramWind <121439140+ProgramWind@users.noreply.github.com>
2025-03-03 10:42:25 +0530

    Added Post Api

--- StoreManagmentApp/src/main/java/com/cjc/main/controller/ProductController.java
+++ StoreManagmentApp/src/main/java/com/cjc/main/controller/ProductController.java
@@ -1,8 +1,17 @@
 package com.cjc.main.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.main.model.Product;
 
 @RestController
 public class ProductController {
+
+@PostMapping("/save")	
+public String saveData(@RequestBody Product p) {
+return "inserted";	
+}
 
 }
